2f553619fe50a1f0f58f8a95e5f64a66bbf37d0b
JagdishMandhalkar <jagdishmandhalkar1508@gmail.com>
2024-05-08 22:47:55 +0530

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,52 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setusers] = useState([]);
 
     useEffect(() => {
-        console.log("kaisa hai bro?");
-    },[] );
+        loadUsers();
+    }, []);
 
-    return(
+    const loadUsers = async () => {
+        const result = await axios.get("http://localhost:3003/users");
+        setusers(result.data);
+    }
+
+    return (
 
         <div className='container'>
             <div className='py-4'>
                 <h1>Home Page</h1>
+                <table className="table border shadow">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th scope='col'>#</th>
+                            <th scope='col'>Name</th>
+                            <th scope='col'>User Name</th>
+                            <th scope='col'>Email</th>
+                            <th>Action</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index)=> (
+                                <tr>
+                                    <th scope='row'>{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <Link></Link>
+                                    </td>
+                                </tr>
+
+                            ))
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
     )
